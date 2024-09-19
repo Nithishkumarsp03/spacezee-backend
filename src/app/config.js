3aa2f83d19c5dc311712c43508a42b9208706be2
mksmmirhs <meraj.exe@gmail.com>
2024-09-20 00:08:01 +0600

    docker final

--- src/app/config.js
+++ src/app/config.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import path from "path";
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+console.log("i am running env: ", process.env.PORT);
+
 export default {
   port: process.env.PORT,
   database_url: process.env.DB_URL,
